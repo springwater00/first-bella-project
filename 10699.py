1906cafb9297bec4ea0232e706a21e24c48fa7f1
Soonju Jeong <9180jsj@naver.com>
2022-06-21 23:31:48 +0800

    backjoon

---
+++ 10699.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime
+from pytz_deprecation_shim import UTC
+import time
+
+print(datetime.)
+print(datetime(1989,1,23,0,0,0,0, UTC(8)))
+# print(datetime.now(tz=UTC 8))
+# print(datetime.now().astimezone[UTC: 8])
+# print(today())
+# # time = date(datetime(2022,6,21).astimezone(UTC,8))
+# print(time)
