31846c34720d5d4f1530c77574145806d04da993
Soonju Jeong <9180jsj@naver.com>
2022-06-22 22:50:56 +0800

    backjoon 2nd commit

--- 10699.py
+++ 10699.py
@@ -1,9 +1,18 @@
-from datetime import date, datetime
+from datetime import date, datetime, timezone, tzinfo, timedelta
 from pytz_deprecation_shim import UTC
 import time
 
-print(datetime.)
-print(datetime(1989,1,23,0,0,0,0, UTC(8)))
+
+# output = datetime.datetime(2007, 12, 6, 15, 29, 43, 79060, timezone=UTC)
+
+output = today()
+# timedelta(hours=8)
+# output = datetime(UTC())
+# output = datetime.tzinfo(UTC)
+
+# output = datetime.datetime.utcnow()
+print(output)
+# print(datetime(1989,1,23,0,0,0,0, UTC(8)))
 # print(datetime.now(tz=UTC 8))
 # print(datetime.now().astimezone[UTC: 8])
 # print(today())
